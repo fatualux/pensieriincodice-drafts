c1a9cd56659fea0e5287245684d301fe7349fd16
fatualux <35587292+fatualux@users.noreply.github.com>
2023-12-08 21:04:01 +0100

    Edited script to generate gallery

--- generte_gallery.sh
+++ generte_gallery.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# Output HTML file
-OUTPUT_HTML="README.md"
+# Output SVG file
+OUTPUT_SVG="gallery.svg"
 
 # Directory containing images
 IMAGES_DIR="images"
 
-# Header of the HTML gallery
-echo "# GALLERY" > "$OUTPUT_HTML"
-echo "" >> "$OUTPUT_HTML"
-echo '<!DOCTYPE html>' >> "$OUTPUT_HTML"
-echo '<html lang="en">' >> "$OUTPUT_HTML"
-echo '<head>' >> "$OUTPUT_HTML"
-echo '  <meta charset="UTF-8">' >> "$OUTPUT_HTML"
-echo '  <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> "$OUTPUT_HTML"
-echo '  <style>' >> "$OUTPUT_HTML"
-echo '    .gallery { display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 10px; }' >> "$OUTPUT_HTML"
-echo '    .thumbnail { width: 100%; height: auto; }' >> "$OUTPUT_HTML"
-echo '  </style>' >> "$OUTPUT_HTML"
-echo '</head>' >> "$OUTPUT_HTML"
-echo '<body>' >> "$OUTPUT_HTML"
-echo '' >> "$OUTPUT_HTML"
-echo '<div class="gallery">' >> "$OUTPUT_HTML"
+# Header of the SVG gallery
+echo '<svg fill="none" viewBox="0 0 800 400" width="800" height="400" xmlns="http://www.w3.org/2000/svg">' > "$OUTPUT_SVG"
+echo '  <foreignObject width="100%" height="100%">' >> "$OUTPUT_SVG"
+echo '    <div xmlns="http://www.w3.org/1999/xhtml">' >> "$OUTPUT_SVG"
+
+# CSS Styles
+echo '      <style>' >> "$OUTPUT_SVG"
+echo '        .gallery { display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 10px; }' >> "$OUTPUT_SVG"
+echo '        .thumbnail { width: 100%; height: auto; }' >> "$OUTPUT_SVG"
+echo '      </style>' >> "$OUTPUT_SVG"
+
+# Opening tag for the gallery
+echo '      <div class="gallery">' >> "$OUTPUT_SVG"
 
 # Loop through each image in the images directory
 for IMAGE in "$IMAGES_DIR"/*; do
@@ -29,15 +26,15 @@ for IMAGE in "$IMAGES_DIR"/*; do
     if [ -f "$IMAGE" ]; then
         # Get the filename
         FILENAME=$(basename "$IMAGE")
-        # Add image to the HTML gallery
-        echo '  <a href="'$IMAGE'" target="_blank">' >> "$OUTPUT_HTML"
-        echo '    <img src="'$IMAGE'" alt="Thumbnail of '$FILENAME'" class="thumbnail">' >> "$OUTPUT_HTML"
-        echo '  </a>' >> "$OUTPUT_HTML"
+        # Add image to the SVG gallery
+        echo '        <a href="'$IMAGE'" target="_blank">' >> "$OUTPUT_SVG"
+        echo '          <img src="'$IMAGE'" alt="Thumbnail of '$FILENAME'" class="thumbnail">' >> "$OUTPUT_SVG"
+        echo '        </a>' >> "$OUTPUT_SVG"
     fi
 done
 
-# Closing tags of the HTML gallery
-echo '</div>' >> "$OUTPUT_HTML"
-echo '' >> "$OUTPUT_HTML"
-echo '</body>' >> "$OUTPUT_HTML"
-echo '</html>' >> "$OUTPUT_HTML"
+# Closing tags for the gallery and SVG
+echo '      </div>' >> "$OUTPUT_SVG"
+echo '    </div>' >> "$OUTPUT_SVG"
+echo '  </foreignObject>' >> "$OUTPUT_SVG"
+echo '</svg>' >> "$OUTPUT_SVG"
