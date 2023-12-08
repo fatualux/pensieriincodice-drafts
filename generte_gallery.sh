d964316e470d17a7c3e18644bf0d573ae5b6ad6c
fatualux <35587292+fatualux@users.noreply.github.com>
2023-12-08 21:43:25 +0100

    Edited script to generate gallery

--- generte_gallery.sh
+++ generte_gallery.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 OUTPUT_HTML="README.md"
+CSS_FILE="style.css"
 
 IMAGES_DIR="images"
 THUMBS_DIR="thumbs"
 
+# Create the CSS file
+echo '  .gallery { display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 10px; }' > "$CSS_FILE"
+echo '  .thumbnail { width: 100%; height: auto; }' >> "$CSS_FILE"
+
+# Create the HTML file
 echo "# GALLERY" > "$OUTPUT_HTML"
 echo "" >> "$OUTPUT_HTML"
 echo '<!DOCTYPE html>' >> "$OUTPUT_HTML"
@@ -12,26 +18,24 @@ echo '<html lang="en">' >> "$OUTPUT_HTML"
 echo '<head>' >> "$OUTPUT_HTML"
 echo '  <meta charset="UTF-8">' >> "$OUTPUT_HTML"
 echo '  <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> "$OUTPUT_HTML"
-echo '  <style>' >> "$OUTPUT_HTML"
-echo '    .gallery { display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 10px; }' >> "$OUTPUT_HTML"
-echo '    .thumbnail { width: 100%; height: auto; }' >> "$OUTPUT_HTML"
-echo '  </style>' >> "$OUTPUT_HTML"
+echo "  <link rel=\"stylesheet\" type=\"text/css\" href=\"$CSS_FILE\">" >> "$OUTPUT_HTML"
 echo '</head>' >> "$OUTPUT_HTML"
 echo '<body>' >> "$OUTPUT_HTML"
 echo '' >> "$OUTPUT_HTML"
 echo '<div class="gallery">' >> "$OUTPUT_HTML"
 
-for IMAGE in "$IMAGES_DIR"/*; do
+# Add thumbnails to the gallery
+for THUMB in "$THUMBS_DIR"/*; do
     # Check if it's a file
-    if [ -f "$IMAGE" ]; then
+    if [ -f "$THUMB" ]; then
         # Get the filename
-        FILENAME=$(basename "$IMAGE")
+        FILENAME=$(basename "$THUMB")
         # Extract the image name without extension
         IMAGE_NAME="${FILENAME%.*}"
-        # Thumbnail path
-        THUMB_PATH="$THUMBS_DIR/$FILENAME"
-        # Add image link to the list
-        echo "  <a href=\"$IMAGE\"><img class=\"thumbnail\" src=\"$THUMB_PATH\" alt=\"$IMAGE_NAME\"></a>" >> "$OUTPUT_HTML"
+        # Original image path with automatic extension detection
+        IMAGE_PATH="$IMAGES_DIR/$IMAGE_NAME.$(file -b --extension "$IMAGES_DIR/$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')"
+        # Add thumbnail link to the list
+        echo "  <a href=\"$IMAGE_PATH\"><img class=\"thumbnail\" src=\"$THUMB\" alt=\"$IMAGE_NAME\"></a>" >> "$OUTPUT_HTML"
     fi
 done
 
